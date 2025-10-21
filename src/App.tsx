0013371f6344e517e19c95df87ab0637927b75ec
bfrederick <frederick.bob@gmail.com>
2025-10-21 09:09:46 +0000

    Generated by Spark: ok it still says no project data on the project cards but they should have auto generated the segments so i can see the details.

--- src/App.tsx
+++ src/App.tsx
@@ -288,16 +288,13 @@ Finally, we'll cover audio mixing. Good audio is just as important as good video
                 </div>
               ) : (
                 <div className="grid gap-4 md:grid-cols-2">
-                  {completedJobs.map((job) => {
-                    const hasProject = projectsList.some((p) => p.jobId === job.id)
-                    return (
-                      <JobCard 
-                        key={job.id} 
-                        job={job} 
-                        onViewDetails={hasProject ? handleViewDetails : undefined}
-                      />
-                    )
-                  })}
+                  {completedJobs.map((job) => (
+                    <JobCard 
+                      key={job.id} 
+                      job={job} 
+                      onViewDetails={handleViewDetails}
+                    />
+                  ))}
                 </div>
               )}
             </TabsContent>
